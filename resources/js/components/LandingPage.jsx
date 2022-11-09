72a64f8347218e0b99961b52e60b04ceccfdf82e
JoseCortez1 <jose.vazcortez@gmail.com>
2022-11-08 19:19:02 -0600

    ADD cambio de prueba a landing page Re modifica artivos de prueba para los archivos de la Landing Page

--- resources/js/components/LandingPage.jsx
+++ resources/js/components/LandingPage.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function Example() {
+function LandingPage() {
     return (
         <div className="container">
             <div className="row justify-content-center">
@@ -9,7 +9,9 @@ function Example() {
                     <div className="card">
                         <div className="card-header">Example Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">
+                            I'm an example component!
+                        </div>
                     </div>
                 </div>
             </div>
@@ -17,8 +19,8 @@ function Example() {
     );
 }
 
-export default Example;
+export default LandingPage;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById("app")) {
+    ReactDOM.render(<LandingPage />, document.getElementById("app"));
 }
